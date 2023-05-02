f3438acb279b2ed1b64f3a86f126d2ca0b67cf6f
heartdoc516 <heartdoc855@gmail.com>
2023-05-02 10:53:50 -0400

    mobile boards menu select bug

--- components/Boards.tsx
+++ components/Boards.tsx
@@ -2,24 +2,14 @@
 
 import Board from "./Board";
 import Link from "next/link";
-import { useEffect, useContext } from "react";
+import { useContext } from "react";
 import NewBoard from "./NewBoard";
-import { useRouter, useSelectedLayoutSegment } from "next/navigation";
+
 import { BoardContext, setBoardContext } from "@/lib/BoardContextProvider";
 
 const Boards = ({ boards }) => {
-  const boardId = useSelectedLayoutSegment();
-  const router = useRouter();
-
   const activeBoard = useContext(BoardContext);
   const dispatch = useContext(setBoardContext);
-
-  useEffect(() => {
-    if (boardId === null && boards.length) {
-      dispatch({ board: boards[0] });
-      router.push(`/dashboard/${boards[0].id}`);
-    }
-  });
 
   const handleSelectBoard = (board) => {
     dispatch({ board });
